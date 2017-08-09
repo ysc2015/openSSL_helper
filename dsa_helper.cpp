552e6de05b951f97e39d2340c11f00a47608b7be
Sadik Younsse <ysc2011.freelancer@gmail.com>
2017-08-09 20:43:22 +0000

    add load_public_key_pem function
    the load_public_key_pem load a dsa public key in pem format

--- dsa_helper.cpp
+++ dsa_helper.cpp
@@ -28,3 +28,32 @@ EVP_PKEY *load_dsa_public_key_der(unsigned char *data, unsigned long size){
 
 	return public_key;
 }
+
+EVP_PKEY *load_dsa_public_key_pem(unsigned char *data, unsigned long size) {
+	BIO *pub_key = BIO_new_mem_buf(data, size);
+	EVP_PKEY *public_key = NULL;
+	
+	if (pub_key == NULL) {
+		return NULL;
+	}
+
+	DSA *dsa = PEM_read_bio_DSA_PUBKEY(pub_key, NULL, NULL, NULL);
+	if (dsa == NULL) {
+		return NULL;
+	}
+	
+	public_key = EVP_PKEY_new();
+	
+	if (!public_key) {
+		BIO_free(pub_key);
+		DSA_free(dsa);
+		return NULL;
+	}
+	
+	EVP_PKEY_set1_DSA(public_key, dsa);
+
+	BIO_free(pub_key);
+	DSA_free(dsa);
+	
+	return public_key;
+}
