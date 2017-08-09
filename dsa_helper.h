99c0f070b5a1f1b11d972800885125755a3011f0
Sadik Younsse <ysc2011.freelancer@gmail.com>
2017-08-09 20:33:47 +0000

    Create dsa_helper header

---
+++ dsa_helper.h
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/evp.h>
+#include <openssl/ossl_typ.h>
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/opensslconf.h>
+
+#ifndef _DSA_HELPER_
+#define _DSA_HELPER_
+
+EVP_PKEY *load_dsa_public_key_der(unsigned char *data, unsigned long size);
+
+#endif
