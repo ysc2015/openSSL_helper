10061275a412598cb25eeb8762718b1e8f475241
Sadik Younsse <ysc2011.freelancer@gmail.com>
2017-08-11 11:14:43 +0000

    add dsa_verify_signature_2

--- dsa_helper.cpp
+++ dsa_helper.cpp
@@ -1,4 +1,16 @@
 #include "dsa_helper.h"
+/*
+If an error occur check calling the 
+OpenSSL_add_all_algorithms();
+and load message so you can know what kind of error using ERR_load_crypto_strings();
+
+To get the error you can use this piece of code 
+
+char buffer[500];
+ERR_error_string(ERR_get_error(), buffer);
+std::cout << " ---- " << buffer << std::endl;
+
+*/
 
 EVP_PKEY *load_dsa_public_key_der(unsigned char *data, unsigned long size){
 	BIO *pub_key = BIO_new_mem_buf(data, size);
@@ -140,5 +152,43 @@ int dsa_verify_signature(
 		}
 	}
 
+	return 0;
+}
+
+int dsa_verify_signature_2(
+	unsigned char *public_key_data, unsigned long public_key_data_size,
+	unsigned char *signature_data, unsigned long signature_size,
+	unsigned char *signed_data, unsigned long signed_data_size)
+{
+	DSA* dsa = NULL;
+	EVP_PKEY* pubKey = NULL;
+
+	pubKey = load_dsa_public_key(public_key_data, public_key_data_size);
+	if (!pubKey)
+	{
+		return 0;
+	}
+
+	EVP_MD_CTX* m_RSAVerifyCtx = EVP_MD_CTX_create();
+
+	if (!m_RSAVerifyCtx) {
+		return 0;
+	}
+
+	if (EVP_DigestVerifyInit(m_RSAVerifyCtx, NULL, EVP_sha1(), NULL, pubKey) <= 0) {
+		return 0;
+	}
+
+	if (EVP_DigestVerifyUpdate(m_RSAVerifyCtx, signed_data, signed_data_size) <= 0) {
+		return 0;
+	}
+
+	int ec = EVP_DigestVerifyFinal(m_RSAVerifyCtx, signature_data, signature_size);
+	if (ec == 1) {
+		EVP_MD_CTX_destroy(m_RSAVerifyCtx);
+		return 1;
+	}
+	
+	EVP_MD_CTX_destroy(m_RSAVerifyCtx);
 	return 0;
 }
